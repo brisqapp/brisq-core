7d3140cb73910356f326cff29baad3dc5a65c104
retroock <olivier.tissot21@gmail.com>
2022-08-25 11:12:06 +0200

    Modification des informations de la DB dans index.js

--- src/db/index.js
+++ src/db/index.js
@@ -3,13 +3,13 @@ import Sequelize from "sequelize";
 export default function connectToBD(){
     
     const PASSWORD_USER = "Pa$$w0rd"
-    const USERNAME = "user"
-    const DATABASE = "brisq"
+    const USERNAME = "brisq"
+    const DATABASE = "brisq_db"
     const HOST = "db_brisq:3306"
 
     const url = "mariadb://" + USERNAME + ":" + PASSWORD_USER + "@" + HOST + "/" + DATABASE;
 
-    const sequelized = new Sequelized(DATABASE, USERNAME, PASSWORD_USER, {
+    const sequelized = new Sequelize(DATABASE, USERNAME, PASSWORD_USER, {
         host: HOST,
         dialect: 'mariadb'
       });
