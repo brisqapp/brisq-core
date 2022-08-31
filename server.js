ae5ead539aadb301b9bb956c94bb8e113fa42942
retroock <olivier.tissot21@gmail.com>
2022-08-31 13:46:48 +0200

    Authentification prête à l'utilisation dans le front-end

--- server.js
+++ server.js
@@ -32,8 +32,10 @@ app.get("/", (req, res) => {
 require("./app/routes/companyType.routes")(app);
 require("./app/routes/company.routes")(app);
 
+
 // set port, listen for requests
 const PORT = process.env.NODE_DOCKER_PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
